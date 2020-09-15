9468df8ebecd8758331bd52ab8bc535135bf5404
Johan Meyer <jjmeyer@live.co.za>
2020-09-15 16:02:03 +0200

    Add FortifyUiServiceProvider and config

---
+++ src/FortifyUiServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ycore\FortifyUi;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Features;
+use Laravel\Fortify\Fortify;
+
+class FortifyUiServiceProvider extends ServiceProvider
+{
+}
