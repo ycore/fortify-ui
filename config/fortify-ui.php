a3817d4e1803165789543019c5292d8eefe28625
Johan Meyer <jjmeyer@live.co.za>
2020-10-10 14:33:35 +0200

    Update config with view locations

--- config/fortify-ui.php
+++ config/fortify-ui.php
@@ -1,4 +1,17 @@
 <?php
 
 return [
+    'views' => [
+        'login' => 'auth.login',
+        'register' => 'auth.register',
+        'verify-email' => 'auth.verify-email',
+        'reset-password' => 'auth.reset-password',
+        'forgot-password' => 'auth.forgot-password',
+        'confirm-password' => 'auth.confirm-password',
+        'two-factor-challenge' => 'auth.two-factor-challenge',
+
+        'update-password' => 'profile.update-password',
+        'two-factor-authentication' => 'profile.two-factor-authentication',
+        'update-profile-information' => 'profile.update-profile-information',
+    ],
 ];
