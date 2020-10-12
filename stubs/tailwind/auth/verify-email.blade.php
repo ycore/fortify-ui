768247b89718c1b86c3e8e220afc58f8bad1d5bb
Johan Meyer <jjmeyer@live.co.za>
2020-10-12 12:42:03 +0200

    Update verify-email stub

--- stubs/tailwind/auth/verify-email.blade.php
+++ stubs/tailwind/auth/verify-email.blade.php
@@ -14,5 +14,25 @@
         </div>
         @endif
 
+        <div class="flex items-center justify-between mt-4">
+            <form method="POST" action="{{ route('verification.send') }}">
+                @csrf
+
+                <div>
+                    <button type="submit"
+                        class="flex justify-center px-2 py-2 text-white uppercase transition duration-300 ease-in-out bg-gray-800 border border-gray-900 rounded-md hover:bg-blue-800 focus:outline-none focus:shadow-outline active:bg-gray-700">
+                        {{ __('Resend Verification Email') }}
+                    </button>
+                </div>
+            </form>
+
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <button type="submit" class="text-gray-700 underline uppercase hover:text-blue-700">
+                    {{ __('Logout') }}
+                </button>
+            </form>
+        </div>
     </div>
 </x-layouts.guest>
