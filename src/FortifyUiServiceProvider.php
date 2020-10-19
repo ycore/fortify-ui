f1eb197b6cabc1ede6a943ac44df9c37e47b9c54
Johan Meyer <jjmeyer@live.co.za>
2020-10-19 18:18:06 +0200

    Implement boot sections using config locations - Add example implementations to the home view

--- src/FortifyUiServiceProvider.php
+++ src/FortifyUiServiceProvider.php
@@ -22,6 +22,18 @@ class FortifyUiServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
+        Fortify::createUsersUsing(CreateNewUser::class);
+
+        Fortify::loginView(function () {
+            return view(config('fortify-ui.views.login'));
+        });
+
+        Fortify::confirmPasswordView(function () {
+            return view(config('fortify-ui.views.confirm-password'));
+        });
+
+        $this->bootFeatures();
+
         if ($this->app->runningInConsole()) {
             $this->configurePublishing();
         }
@@ -35,6 +47,52 @@ class FortifyUiServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/fortify-ui.php', 'fortify-ui');
+    }
+
+    /**
+     * Boot enabled featured.
+     *
+     * @return void
+     */
+    protected function bootFeatures(): void
+    {
+        if (Features::enabled(Features::resetPasswords())) {
+            Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+            Fortify::requestPasswordResetLinkView(function () {
+                return view(config('fortify-ui.views.forgot-password'));
+            });
+
+            Fortify::resetPasswordView(function ($request) {
+                return view(config('fortify-ui.views.reset-password'), ['request' => $request]);
+            });
+        }
+
+        if (Features::enabled(Features::registration())) {
+            Fortify::registerView(function () {
+                return view(config('fortify-ui.views.register'));
+            });
+        }
+
+        if (Features::enabled(Features::emailVerification())) {
+            Fortify::verifyEmailView(function () {
+                return view(config('fortify-ui.views.verify-email'));
+            });
+        }
+
+        if (Features::enabled(Features::updateProfileInformation())) {
+            Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        }
+
+        if (Features::enabled(Features::updatePasswords())) {
+            Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        }
+
+        if (Features::enabled(Features::twoFactorAuthentication())) {
+            Fortify::twoFactorChallengeView(function () {
+                return view(config('fortify-ui.views.two-factor-challenge'));
+            });
+        }
     }
 
     /**
--- stubs/tailwind/home.blade.php
+++ stubs/tailwind/home.blade.php
@@ -15,4 +15,75 @@
 
         </form>
     </div>
+    {{-- EXAMPLE: this section contains forms and routes not readily accesible upon intital installation. It is meant to be repalced --}}
+    <section class="flex flex-col px-4 pt-12 mx-auto">
+        <div class="py-2">
+            <p class="text-yellow-400">EXAMPLE: This section includes examples of the forms that can be implemented for each of the selected features</p>
+        </div>
+        @php
+            $views['update-profile-information'] = config('fortify-ui.views.update-profile-information');
+            $views['update-password'] = config('fortify-ui.views.update-password');
+            $views['two-factor-authentication'] = config('fortify-ui.views.two-factor-authentication');
+        @endphp
+        <div class="flex flex-wrap justify-between mx-auto space-x-8">
+            <div class="space-y-4">
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updateProfileInformation()))
+                    <div>
+                        @include ($views['update-profile-information'])
+                    </div>
+                @endif
+
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                    <div>
+                        @include($views['update-password'])
+                    </div>
+                @endif
+            </div>
+            <div class="space-y-4">
+                @if (Route::has('password.confirm'))
+                <div>
+                    <a href="{{ route('password.confirm') }}"
+                        class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                        {{ __('Confirm your password?') }}
+                    </a>
+                </div>
+                @endif
+                @if (Route::has('password.confirmation'))
+                <div>
+                    <a href="{{ route('password.confirmation') }}"
+                        class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                        {{ __('Password confirmation status') }}
+                    </a>
+                </div>
+                @endif
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()))
+                <div>
+                    <a href="{{ route('verification.notice') }}"
+                        class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                        {{ __('Email verification') }}
+                    </a>
+                </div>
+                @endif
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::twoFactorAuthentication()))
+                    @if(auth()->user()->two_factor_recovery_codes)
+                    <div>
+                        <a href="{{ url('/user/two-factor-qr-code') }}"
+                            class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                            {{ __('Two factor QR code') }}
+                        </a>
+                    </div>
+                    <div>
+                        <a href="{{ url('/user/two-factor-recovery-codes') }}"
+                            class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                            {{ __('Two factor recovery codes') }}
+                        </a>
+                    </div>
+                    @endif
+                    <div>
+                        @include($views['two-factor-authentication'])
+                    </div>
+                @endif
+            </div>
+        </div>
+    </section>
 </x-layouts.guest>
